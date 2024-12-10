177acddbdc4d8bb988e95716fb4f4eab708a36b1
Brandon Leon <brandonleon@pm.me>
2024-12-10 15:17:40 -0800

    ✨ fix: simplify phonetic representation function signature
    Refactor the `get_phonetic_representation` function to remove the 
    unnecessary `plain` parameter. Update the `transform_text_to_nato_phonetic` 
    function to reflect this change. Also, correct the module docstring to 
    capitalize "Phonetik" for consistency.

--- src/phonetik/main.py
+++ src/phonetik/main.py
@@ -1,4 +1,4 @@
-"""Main module for the phonetik application."""
+"""Main module for the Phonetik application."""
 
 import sys
 from importlib.metadata import PackageNotFoundError
@@ -30,7 +30,7 @@ def version_callback(value: bool):
         raise typer.Exit()
 
 
-def get_phonetic_representation(letter: str, plain: bool) -> Text:
+def get_phonetic_representation(letter: str) -> Text:
     """Get the phonetic representation of a letter."""
     upper_letter = letter.upper()
     if upper_letter in nato_phonetic_alphabet:
@@ -46,7 +46,8 @@ def get_phonetic_representation(letter: str, plain: bool) -> Text:
 def transform_text_to_nato_phonetic(text: str, plain: bool, single_line: bool) -> None:
     """Transform text into the NATO phonetic alphabet."""
     console = Console(no_color=plain)
-    result = [get_phonetic_representation(letter, plain) for letter in text]
+    result = [get_phonetic_representation(letter) for letter in text]
+
     if single_line:
         console.print(*result, sep=" ")
     else:
