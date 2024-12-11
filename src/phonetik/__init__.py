98c2cc7761138f13ae687318a7eb93784016ee93
Brandon Leon <brandon.leon@servicenow.com>
2024-12-11 14:31:19 -0800

    ✨ feat: update dependencies and add tests for phonetik
    Add new development dependencies including `pytest` 
    `pytest-sugar` for improved testing experience. Update the 
    current version of the project to 1.3.4. Introduce new 
    packages such as `pluggy`, `packaging`, `iniconfig`, and 
    `termcolor`. Implement unit tests for the `get_phonetic_representation` 
    function to ensure correct functionality.

--- src/phonetik/__init__.py
+++ src/phonetik/__init__.py
@@ -1,3 +1,5 @@
 """Phonetik package."""
 
 from .main import app as app
+from .main import get_phonetic_representation as get_phonetic_representation
+from .main import transform_text_to_nato_phonetic as transform_text_to_nato_phonetic
